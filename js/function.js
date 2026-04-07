1a8864590efcf9dfd6d39983a5d9d273f48ed347
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-07 17:44:37 +0200

    Aggiunta funzione per la creazione di 5 numeri casuali

--- js/function.js
+++ js/function.js
@@ -1,4 +1,22 @@
 //Funzione per la creazione di numeri casuali
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Funzione per la creazione di 5 numeri casuali 
+function getUniqueRandomNumbers(total, min, max) {
+    //Creazione di un array per conservare i numeri
+    const numbers = [];
+
+    //Ciclo WHILE che genera numeri fino al totale desiderato
+    while (numbers.length < total) {
+        const randomNumber = getRandomNumber(min, max);
+
+        //Controllo tramite IF per evitare di avere numeri uguali all'interno dell'array
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+
+    return numbers;
 }
