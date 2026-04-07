4ba5da2c58c3f178e53c123096fd220f92f0b666
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-07 17:34:01 +0200

    Recupero dei dati dall'html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Recupero dei dati dall'html
+const countdownElement = document.getElementById("countdown");
+const instructionsElement = document.getElementById("instructions");
+const numbersListElement = document.getElementById("numbers-list");
+const answersFormElement = document.getElementById("answers-form");
+const messageElement = document.getElementById("message");
+const inputElements = document.querySelectorAll("#input-group input");
