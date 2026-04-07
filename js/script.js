cd42d7d47e73c77b988f0cb7147d63d65d80960c
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-07 17:54:31 +0200

    Aggiunto richiamo della funzione per generare un gruppo di numeri random in script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ const numbersListElement = document.getElementById("numbers-list");
 const answersFormElement = document.getElementById("answers-form");
 const messageElement = document.getElementById("message");
 const inputElements = document.querySelectorAll("#input-group input");
+
+//Richiamo della funzione per generare un gruppo di numeri random
+const secretNumbers = getUniqueRandomNumbers(5, 1, 100);
+
+console.log(secretNumbers);
